cc0f83a2f5402fd1a8309a4dc35f8bb211ec993c
DevTaissirHammouda <taissir7ammouda@email.com>
2025-07-22 23:00:43 +0100

    feat(matcher_bert): save job embeddings and data to disk
    Add functionality to persist job embeddings and job data to files for future use

--- AiRanker/matcher_bert.py
+++ AiRanker/matcher_bert.py
@@ -6,5 +6,8 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 def compute_bert_similarity(resumes, jobs):
     resume_embeddings = model.encode(resumes.tolist(), convert_to_numpy=True)
     job_embeddings = model.encode(jobs.tolist(), convert_to_numpy=True)
-
+    bert_job_embeddings = model.encode(jobs_df['cleaned_job_desc'].tolist(), show_progress_bar=True)
+    # Save to disk
+    np.save("AiRanker/output/job_bert_vectors.npy", bert_job_embeddings)
+    jobs_df.to_csv("AiRanker/output/jobs.csv", index=False)
     return cosine_similarity(resume_embeddings, job_embeddings)

cf7bbbcec2d74c7fbb89370010ac7f257ea88d27
DevTaissirHammouda <taissir7ammouda@email.com>
2025-07-22 22:02:58 +0100

    feat: add resume-job matching functionality with similarity scores
    Implement compute_similarity function using TF-IDF and cosine similarity
    Add logic to display top 3 job matches for each resume with scores

--- AiRanker/main.py
+++ AiRanker/main.py
@@ -2,7 +2,8 @@
 
 from data_loader import *
 from utils import *
-
+from matcher import compute_similarity
+import numpy as np
 resumes_df = load_resumes('AiRanker/data/resumes/resumes.csv')
 jobs_df = load_jobs('AiRanker/data/jobs/jobs.csv')
 
@@ -11,5 +12,19 @@ jobs_df = load_jobs('AiRanker/data/jobs/jobs.csv')
 resumes_df['cleaned_resume'] = resumes_df['Resume'].apply(preprocess_text)
 jobs_df['cleaned_job_desc'] = jobs_df['Job Description'].apply(preprocess_text)
 
-print(resumes_df[['Category', 'cleaned_resume']].head())
-print(jobs_df[['Job Title', 'cleaned_job_desc']].head())
+similarity = compute_similarity(resumes_df['cleaned_resume'], jobs_df['cleaned_job_desc'])
+
+
+
+
+
+top_n = 3
+
+for resume_idx in range(len(resumes_df)):
+    top_indices = np.argsort(similarity[resume_idx])[::-1][:top_n]
+    print(f"\nTop {top_n} job matches for Resume {resume_idx} ({resumes_df.iloc[resume_idx]['Category']}):")
+
+    for i, job_idx in enumerate(top_indices):
+        score = similarity[resume_idx][job_idx]
+        title = jobs_df.iloc[job_idx]['Job Title']
+        print(f"{i+1}. {title} (Score: {score:.2f})")
